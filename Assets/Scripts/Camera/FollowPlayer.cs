867d69e722b12fd5423cbd08806fd6490b868a01
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2023-08-31 16:51:44 +0800

    Added camera follow
    Created camera follow that follows player movements

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Vector3 offset;
+    private Vector3 currentVelocity = Vector3.zero;
+
+    public Transform target;
+
+    [SerializeField] float smoothTime = 0.25f;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+    void FixedUpdate()
+    {
+        var targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
+    }
+}
