5f370c563263c3139e20d167e88cbb0b0d092b94
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2023-08-31 20:19:23 +0800

    Cooldown for shifting
    Added cooldown for shifting

--- Assets/Scripts/Player/Shift.cs
+++ Assets/Scripts/Player/Shift.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Shift : MonoBehaviour
 {
     MovementScript moveScript;
-    
+
     [Header("Shift Force")]
+    bool canShift = true;
     [SerializeField] float shiftForce = 10f;
     [SerializeField] float shiftTime = 0.8f;
+
+    [Header("Cooldown")]
+    [SerializeField] float cooldownTime = 1.0f;
     
     void Start()
     {
@@ -18,21 +22,29 @@ public class Shift : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canShift == true)
+        {  
             StartCoroutine(DashCoroutine());
         }
-
     }
 
-
+    // Coroutine that dashes player based on dash time
+    // After dashing, an X second cooldown is placed
     IEnumerator DashCoroutine()
     {
+        canShift = false;
+
         float startTime = Time.time;
+
         while (Time.time < startTime + shiftTime)
         {
             moveScript.player.Move(transform.forward * shiftForce * Time.deltaTime);
-            yield return null; 
+            yield return null;
         }
-}
+
+        // After cooldown ends, player is able to dash again
+        yield return new WaitForSeconds(cooldownTime);
+        canShift = true;
+
+    }
 }
