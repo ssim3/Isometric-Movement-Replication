df7d1b11bb2f0fa12c88b76800885a6433d67853
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2023-09-13 14:27:04 +0800

    metadata changes

---
+++ Library/PackageCache/com.unity.services.cloudsave@2.2.1/Runtime/CloudSaveService.cs
@@ -0,0 +1,50 @@
+using Unity.Services.Core;
+
+namespace Unity.Services.CloudSave
+{
+    /// <summary>
+    /// The entry class to the Cloud Save service.
+    /// </summary>
+    public static class CloudSaveService
+    {
+        internal static ICloudSaveService instance;
+
+        /// <summary>
+        /// The default singleton instance to access the Cloud Save service.
+        /// </summary>
+        /// <exception cref="ServicesInitializationException">
+        /// This exception is thrown if the <code>UnityServices.InitializeAsync()</code>
+        /// has not finished before accessing the singleton.
+        /// </exception>
+        public static ICloudSaveService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    throw new ServicesInitializationException("The Cloud Save service has not been initialized. Please initialize Unity Services.");
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    public class CloudSaveServiceInstance : ICloudSaveService
+    {
+        public ICloudSaveDataClient Data { get; internal set; }
+        public ICloudSaveFilesClient Files { get; internal set; }
+
+        internal CloudSaveServiceInstance(ICloudSaveDataClient data, ICloudSaveFilesClient files)
+        {
+            Data = data;
+            Files = files;
+        }
+    }
+
+    public interface ICloudSaveService
+    {
+        ICloudSaveDataClient Data { get; }
+        ICloudSaveFilesClient Files { get; }
+    }
+}
